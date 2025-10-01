21202b2285f09e2bd2c71860f69477984c7563a2
byte206 <gabimagister@Outlook.es>
2025-10-01 20:11:07 +0200

    que dificil es esto

--- client.c
+++ client.c
@@ -12,7 +12,35 @@
 
 #include "minitalk.h"
 
+void    send_len(int str_len, int server_pid)
+{
+    int i;
+
+    i = 31;
+    while (i >= 0)
+    {
+        if (str_len & (1ULL << i))
+            if (kill(server_pid, SIGUSR1) == -1)
+                ft_printf("Error sending signal\n");
+        else
+            if (kill(server_pid, SIGUSR2) == -1)
+                ft_printf("Error sending signal\n");
+        i--;
+        usleep(500 * 500);
+    }
+}
+
 int main(int argc, char **argv)
 {
+    int server_pid;
+    int str_len;
 
+    if (argc != 3)
+    {
+        ft_printf("Usage: ./client [SERVER_PID] [MESSAGE]\n");
+        return 1;
+    }
+    server_pid = ft_atoi(argv[1]);
+    str_len = ft_strlen(argv[2]);
+    send_len(str_len, server_pid);
 }
